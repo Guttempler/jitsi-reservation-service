8d22122e1e7ef6a234c8667facbe4ae8f3b656b2
Sven Ruppert <sven.ruppert@gmail.com>
2019-12-16 18:26:45 +0100

    dummy reservation

--- src/main/java/org/rapidpm/demo/jitsi/reservation/ReservationService.java
+++ src/main/java/org/rapidpm/demo/jitsi/reservation/ReservationService.java
@@ -62,9 +62,9 @@ public class ReservationService {
 
     Handler handler = ctx -> {
 
-      String name      = ctx.queryParam("name");
-      String startTime = ctx.queryParam("start_time");
-      String mailOwner = ctx.queryParam("mail_owner");
+      String name      = ctx.pathParam("name");
+      String startTime = ctx.pathParam("start_time");
+      String mailOwner = ctx.pathParam("mail_owner");
       logger.info("ctx.name " + name);
       logger.info("ctx.start_time " + startTime);
       logger.info("ctx.mail_owner " + mailOwner);
