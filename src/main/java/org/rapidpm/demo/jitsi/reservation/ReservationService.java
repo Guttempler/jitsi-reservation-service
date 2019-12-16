dfea8375b455a8738ce5f4b6018379cf502c20b0
Sven Ruppert <sven.ruppert@gmail.com>
2019-12-16 18:13:57 +0100

    dummy reservation

--- src/main/java/org/rapidpm/demo/jitsi/reservation/ReservationService.java
+++ src/main/java/org/rapidpm/demo/jitsi/reservation/ReservationService.java
@@ -1,10 +1,17 @@
 package org.rapidpm.demo.jitsi.reservation;
 
 import io.javalin.Javalin;
+import io.javalin.http.Handler;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.rapidpm.dependencies.core.logger.Logger;
+import org.rapidpm.dependencies.core.logger.LoggingService;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
 
 import static java.lang.System.setProperty;
 
@@ -22,6 +29,9 @@ public class ReservationService {
 
 
   public static final String CONFERENCE = "conference";
+  public static final String SDF = "yyyy-MM-dd'T'HH:mm:ss.SSSX";
+public static final SimpleDateFormat formatter = new SimpleDateFormat(SDF);
+
 
   private ReservationService() {
   }
@@ -49,8 +59,29 @@ public class ReservationService {
     Javalin app = Javalin.create()
                          .start(Integer.parseInt(System.getProperty(CORE_REST_SERVER_PORT)));
 
-    app.get("/" + CONFERENCE, ctx -> {
-      ctx.result("Hello World");
-    });
+    LoggingService logger = Logger.getLogger(ReservationService.class);
+
+    Handler handler = ctx -> {
+
+      String name       = ctx.queryParam("name");
+      String start_time = ctx.queryParam("start_time");
+      String mail_owner = ctx.queryParam("mail_owner");
+      logger.info("ctx.name " + name);
+      logger.info("ctx.start_time " + start_time);
+      logger.info("ctx.mail_owner " + mail_owner);
+
+      ctx.result("{\n"
+                 + "  'id': 364758328,\n"
+                 + "  'name': 'conference1234',\n"
+                 + "  'mail_owner': 'user@server.com',\n"
+                 + "  'start_time': '"
+                 + formatter.format(new Date())
+                 + "',\n"
+                 + "  'duration': 120\n"
+                 + "}");
+    };
+    app.get("/" + CONFERENCE, handler);
+
+    app.post("/" + CONFERENCE, handler);
   }
 }
